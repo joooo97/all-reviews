8f668cf33b80d21ac8be0608b7de6e44ca509488
joooo97 <68749697+joooo97@users.noreply.github.com>
2022-12-26 14:27:01 +0900

    Initial commit

---
+++ src/main/java/com/joo/allreviews/AllreviewsApplication.java
@@ -0,0 +1,13 @@
+package com.joo.allreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllreviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllreviewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joo/allreviews/AllreviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joo.allreviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllreviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
